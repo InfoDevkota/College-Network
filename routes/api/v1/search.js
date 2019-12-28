3ed094c7e63291a8d11856ce09835a8d2dd7b9c6
Sagar Devkota <info@devkotasagar.com.np>
2019-12-28 21:56:01 +0545

    Search and auth fix wrong pass

--- routes/api/v1/search.js
+++ routes/api/v1/search.js
@@ -6,7 +6,9 @@ const isAuth = require("../../../auth/is-auth-api");
 const router = express.Router();
 
 router.get("/search/note", searchController.getSearchByNote);
-router.get("/search/post", searchController.getSearchByPost)
+router.get("/search/post", searchController.getSearchByPost);
+router.get("/search/userByDep", searchController.getUserByDepartment);
+router.get("/search/user", searchController.getUser);
 router.get("/search", searchController.getSearch);
 
 
